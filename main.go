15f89710c378f07d6683593a51747bdf706dcadd
tiaszz <teteurezende3@gmail.com>
2026-05-29 14:37:18 -0300

    getting client and updating client

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/create", CreateClient).Methods("POST")
 	router.HandleFunc("/client/{id}", GetClient).Methods("GET")
+	router.HandleFunc("/client/update/{id}", GetClient).Methods("GET")
 
 	srv := &http.Server{
 		Handler: router,
@@ -66,5 +67,31 @@ func GetClient(w http.ResponseWriter, r *http.Request) {
 	}
 
 	client := clients[id-1]
+	w.WriteHeader(http.StatusFound)
 	json.NewEncoder(w).Encode(client)
+}
+
+func UpdateClient(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	clientID := vars["id"]
+
+	id, err := strconv.Atoi(clientID)
+	if err != nil {
+		http.Error(w, "Error converting string to int", http.StatusInternalServerError)
+		return
+	}
+
+	var updatedClient Client
+
+	err = json.NewDecoder(r.Body).Decode(&updatedClient)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	clients[id-1] = updatedClient
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatedClient)
 }
